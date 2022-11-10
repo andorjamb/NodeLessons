3eb95dcff945ce5b93fe941f9ee94379878153df
Anna Petelin <adpetelin@gmail.com>
2022-11-10 15:24:09 +0200

    retry importing path module

--- person_register/index.js
+++ person_register/index.js
@@ -1,8 +1,8 @@
 'use stict';
 
 const http = require('http');
-const path = require('path');
-const {sendFile } = require('./library/utilities');
+import * as path from 'path';
+const { sendFile } = require('./library/utilities');
 const { search } = require('./storage/personsDataLayer');
 
 
@@ -11,7 +11,7 @@ const host = process.env.HOST || 'localhost';
 
 const homePath = path.join(_dirname, 'home.html');
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     const {
         pathname,
         searchParams
@@ -19,33 +19,33 @@ const server = http.createServer((req,res)=>{
 
     const route = decodeURIComponent(pathname);
 
-    if(route===('/')){
+    if (route === ('/')) {
 
     }
-    else if (route.startsWith('/styles')){
-sendFile(res, path.join(_dirname, route), 'text/css')
+    else if (route.startsWith('/styles')) {
+        sendFile(res, path.join(_dirname, route), 'text/css')
     }
-    else if (route.startsWith('/js/')){
+    else if (route.startsWith('/js/')) {
         sendFile(res, path.join(_dirname), route, 'text/javascript');
 
     }
     else {
-        let result=[];
-        if(route==='/persons'){
-            result=search();
+        let result = [];
+        if (route === '/persons') {
+            result = search();
         }
-        else if (route='/persons/firstname'){
-            result=search('firstname', searchParams.get('value'))
+        else if (route = '/persons/firstname') {
+            result = search('firstname', searchParams.get('value'))
         }
-        else if(route==='/persons/age'){
-            result= search('age', searchParams.get('value'))
+        else if (route === '/persons/age') {
+            result = search('age', searchParams.get('value'))
         }
         else {
-            result = { message : 'key not found '}
+            result = { message: 'key not found ' }
         }
-        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(result));
     }
 })
 
-server.listen(port, host, ()=>console.log('server is listening on port ${port}'));
+server.listen(port, host, () => console.log('server is listening on port ${port}'));
